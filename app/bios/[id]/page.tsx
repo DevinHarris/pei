c4cbe526567d7b999e06f362fbd910c070d851aa
Devonta Harris <hello@devinhrrs.me>
2024-07-10 13:41:49 -0400

    various updates

--- app/bios/[id]/page.tsx
+++ app/bios/[id]/page.tsx
@@ -32,6 +32,7 @@ export default function BioPage() {
                             </div>
                         </div>
                         <div className="project-page__meta-grid-col project-page__meta-grid-text">
+                        <h3 className="bio-page__about-heading">About Devonta</h3>
                                 <p className="project-page__meta-text">Devonta is a Full Stack Engineer based in Atlanta, GA, with over three years of experience in web development, database design, and systems architecture. Skilled in technologies like HTML, CSS, JavaScript, React, Node.js</p> 
                                 
                                 <p className="project-page__meta-text">His commitment to excellence is evident in his hands-on approach to problem-solving and his ability to thrive in dynamic environments. He has successfully collaborated with teams to confirm creative proposals, design best practices, and deliver high-quality results. His proactive nature and dedication to continuous improvement make him a valuable asset to any team looking to innovate and excel.</p>
